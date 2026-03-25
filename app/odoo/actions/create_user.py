c0a2789c17606409b56d65cb4f1b3597231ebb3f
HussienSayed14 <hussiens399@gmail.com>
2026-03-25 22:12:41 +0200

    bug

--- app/odoo/actions/create_user.py
+++ app/odoo/actions/create_user.py
@@ -33,6 +33,6 @@ class CreateUserAction(BaseAction):
             "name": self.row.name,
             "login": self.row.email,
             "email": self.row.email,
-            "groups_ids": [(4, group_id)],
+            "group_ids": [(4, group_id)],
         }
         return self.client.execute("res.users", "create", [payload])
